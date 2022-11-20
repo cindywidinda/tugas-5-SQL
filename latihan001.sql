3d2643112a4bff7490974786afba9c7553743a5c
Tribe <cindywidinda@gmail.com>
2022-11-20 18:14:37 +0700

    query pada tabel latihan001

---
+++ latihan001.sql
@@ -0,0 +1,7 @@
+-- Active: 1668941950277@@127.0.0.1@5432@db_latihan001@public
+SELECT * FROM tbl_users;
+-- INSERT INTO tbl_users (username, email, password)
+-- VALUES ('rizkitaa0', 'rizk_kyta1@yahoo.com', 497);
+-- UPDATE tbl_users SET address='buah batu' WHERE id=5;
+-- DELETE FROM tbl_users WHERE email LIKE '%@email.com';
+-- DELETE FROM tbl_users WHERE id=6;
